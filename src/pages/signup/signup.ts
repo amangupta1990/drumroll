10b976449baf5f4b86bafef85d8c77ab517ad923
Aman Gupta <amangupta.1990@ymail.com>
2017-12-04 13:42:51 +0530

    added pager

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -9,7 +9,10 @@ import { NavController, Slides } from 'ionic-angular';
 export class SignupPage {
 
   /** regex for phone number */
-  mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ',' ', /\d/, /\d/, /\d/, ' ','-',' ', /\d/, /\d/, /\d/, /\d/];
+  mask:any[] = ['(', /[1-9]/, /\d/, /\d/, ')', ' ',' ', /\d/, /\d/, /\d/, ' ','-',' ', /\d/, /\d/, /\d/, /\d/];
+  userMobile:any = null;
+  currentPage:number = 1;
+  maxPages:number= 2; 
   private startSlide:number = 0;
   @ViewChild('signupSlides') slider:Slides;
   constructor(public navCtrl: NavController) {
@@ -19,6 +22,25 @@ export class SignupPage {
   ngAfterViewInit(){
     this.slider.freeMode = false;
     this.slider.lockSwipes(true);
+
+    
+    
+  }
+
+  
+
+  pager(){
+    this.currentPage++;
+  
+  }
+
+  getOtp(){
+    //TODO: get send te mobile otp and continue to next slide
+    let phone = parseInt( this.userMobile.replace(/\D+/g, ''))
+    this.slider.lockSwipes(false);
+    this.slider.slideNext(500);
+    this.slider.lockSwipes(true);
+    this.currentPage = 2;
   }
 
 }
